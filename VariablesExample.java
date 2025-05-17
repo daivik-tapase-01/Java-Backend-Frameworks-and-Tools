721b8ad50663ad9f9120824911b74821dde9f428
daivik-tapase-01 <daiviktapase@gmail.com>
2025-05-17 18:07:50 +0530

    Initial commit

---
+++ VariablesExample.java
@@ -0,0 +1,25 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 25;
+        
+        // Floating-point variable
+        double price = 99.99;
+        
+        // Character variable
+        char grade = 'A';
+        
+        // Boolean variable
+        boolean isJavaFun = true;
+        
+        // String variable
+        String name = "Daivik";
+
+        // Printing values
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Name: " + name);
+    }
+}
