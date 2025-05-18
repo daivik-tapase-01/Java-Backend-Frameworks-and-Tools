65c45dbd07c44d4e7db725ade2f21aee4010d7d4
daivik-tapase-01 <daiviktapase@gmail.com>
2025-05-18 23:52:26 +0530

    Conditional Statements

---
+++ ConditionalStatement.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class ConditionalStatement{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // int age = sc.nextInt();
+        // if(age>18){
+        //     System.out.println("Adult");
+        // }else {
+        //     System.out.println("Not Adult");
+        // }
+
+        // int num = sc.nextInt();
+        // if (num % 2==0){
+        //     System.out.println("Even");
+        // }else{
+        //     System.out.println("odd");
+        // }
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // if(a==b){
+        //     System.out.println("a is equal to b"); 
+        // }else if(a>b){
+        //     System.out.println("a is greater");
+        // }else{
+        //     System.out.println("a is smaller");
+        // }
+        
+
+        // int button = sc.nextInt();
+        // if (button == 1){
+        //     System.out.println("Hello");
+        // }else if(button == 2){
+        //     System.out.println("Namaste");
+        // }else if(button == 3){
+        //     System.out.println("Bonjour");
+        // }else{
+        //     System.out.println("Invalid Button");
+        // }
+
+        
+        // int button = sc.nextInt();
+        // switch(button){
+        //     case 1: System.out.println("Hello");
+        //     break;
+
+        //     case 2: System.out.println("Namesta");
+        //     break;
+
+        //     case 3: System.out.println("Bonjour");
+        //     break;
+
+        //     default: System.out.println("Invalid button");
+        // }
+
+        
+    }
+}
