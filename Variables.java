563ed31e75b16b7e8da904b89b23bc1af7a99876
daivik-tapase-01 <daiviktapase@gmail.com>
2025-05-18 10:17:29 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        // String name = "Tony Starks";
+        // int a = 25;
+        // int b = 19;
+        // int age = 20;
+        // double price = 25.25;
+
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        // int diff = a - b;
+        // System.out.println(diff);
+
+        // // Input
+        // Scanner sc = new Scanner(System.in);
+        // String fullname = sc.nextLine();
+        // System.out.println(fullname);
+
+        //Sum two numbers
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
