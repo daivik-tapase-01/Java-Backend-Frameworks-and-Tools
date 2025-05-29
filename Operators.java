e1439db6cb31fee204e2f81b1efad1356bfde2f4
daivik-tapase-01 <daiviktapase@gmail.com>
2025-05-29 18:44:04 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,26 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators ->
+        // Binary + - * / %
+        // Unary ++ --
+
+        // Relational Operators
+        // ==   !=   >   <   >=  <=
+
+        // Logical Operators
+        // &&   ||  !
+
+        // Bitwise Operators
+        // & -> all true so true
+        // | -> one true so true
+        // ^ -> if not similar then true
+        // ~ -> opposite
+        // << -> binary left shift
+        // >> -> binary right shift
+
+        // Assignment Operators
+        // =  +=  -=  *=  /=  
+
+    }
+}
