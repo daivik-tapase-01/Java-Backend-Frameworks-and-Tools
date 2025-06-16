9e2dd1f687145006e928620f7e22112b819a9d64
daivik-tapase-01 <daiviktapase@gmail.com>
2025-06-16 23:59:39 +0530

    Sudoku solver

---
+++ src/javabasics/SudokuSolver.java
@@ -0,0 +1,75 @@
+package javabasics;
+
+public class SudokuSolver {
+
+    public boolean isSafe(char[][] board, int row, int col, int number){
+
+        for ( int i = 0; i < board.length-1; i++){
+            // row & column
+            if(board[i][col] == (char)(number + '0')){
+                return false;
+            }
+            if(board[row][i] == (char)(number + '0')){
+                return false;
+            }
+        }
+
+        //grid
+        int sr = (row/3) * 3;
+        int sc = (col/3) * 3;
+
+        for (int i = sr; i < sr+3; i++) {
+            for (int j = sc; j < sc+3; j++) {
+                if (board[i][j] == (char)(number + '0')){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+
+    }
+
+    public boolean helper(char[][] board, int row , int col){
+
+        if(row == board.length){
+            return true;
+        }
+
+        int nrow = 0;
+        int ncol = 0;
+        if ( col != board.length - 1){
+            nrow = row;
+            ncol = col + 1;
+        }else{
+            nrow = row + 1;
+            ncol = 0;
+        }
+
+        if( board[row][col] != '.'){
+            if(helper(board, nrow, ncol)){
+                return true;
+            }
+        }else{
+            for (int i = 0; i <= 9; i++) {
+                if(isSafe(board, row, col, i)){
+                    board[row][col] = (char)(i + '0');
+                    if(helper(board, nrow, ncol)){
+                        return true;
+                    }else{
+                        board[row][col] = '.';
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        char[][] board;
+        board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
+        helper(board , 0, 0);
+    }
+}
