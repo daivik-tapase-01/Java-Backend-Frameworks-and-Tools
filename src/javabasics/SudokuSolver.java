183b9bd5deb3d4e14a64db4024155cc7eeafe9ed
daivik-tapase-01 <daiviktapase@gmail.com>
2025-06-17 00:08:13 +0530

    Sudoku Solver

--- src/javabasics/SudokuSolver.java
+++ src/javabasics/SudokuSolver.java
@@ -2,9 +2,9 @@ package javabasics;
 
 public class SudokuSolver {
 
-    public boolean isSafe(char[][] board, int row, int col, int number){
+    public static boolean isSafe(char[][] board, int row, int col, int number){
 
-        for ( int i = 0; i < board.length-1; i++){
+        for ( int i = 0; i < board.length; i++){
             // row & column
             if(board[i][col] == (char)(number + '0')){
                 return false;
@@ -30,7 +30,7 @@ public class SudokuSolver {
 
     }
 
-    public boolean helper(char[][] board, int row , int col){
+    public static boolean helper(char[][] board, int row, int col){
 
         if(row == board.length){
             return true;
@@ -51,7 +51,7 @@ public class SudokuSolver {
                 return true;
             }
         }else{
-            for (int i = 0; i <= 9; i++) {
+            for (int i = 1; i <= 9; i++) {
                 if(isSafe(board, row, col, i)){
                     board[row][col] = (char)(i + '0');
                     if(helper(board, nrow, ncol)){
@@ -68,8 +68,26 @@ public class SudokuSolver {
 
 
     public static void main(String[] args) {
-        char[][] board;
-        board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
         helper(board , 0, 0);
+
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
