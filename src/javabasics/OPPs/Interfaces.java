4affc98d7403a8901d891acd752ab7a6858265a6
daivik-tapase-01 <daiviktapase@gmail.com>
2025-06-20 14:41:02 +0530

    Interface

--- src/javabasics/OPPs/Interfaces.java
+++ src/javabasics/OPPs/Interfaces.java
@@ -1,8 +1,29 @@
 package javabasics.OPPs;
 
 interface Animals{
+    void walk();
+}
 
+interface Herbivore{
+    void eats();
+}
+
+class Goat implements Animals, Herbivore{
+    @Override
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+
+    @Override
+    public void eats() {
+        System.out.println("eats grass");
+    }
 }
 
 public class Interfaces {
+    public static void main(String[] args) {
+        Goat d1 = new Goat();
+        d1.walk();
+        d1.eats();
+    }
 }
